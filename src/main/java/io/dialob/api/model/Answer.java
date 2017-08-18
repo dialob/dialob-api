cda8ed84e867c809fb8a9312de04f550da6e08b5
Tomi Pakarinen <tomi.pakarinen@resys.io>
2017-08-18 16:20:32 +0300

    Added updated timestamp and userId on answer

--- src/main/java/io/dialob/api/model/Answer.java
+++ src/main/java/io/dialob/api/model/Answer.java
@@ -16,16 +16,24 @@
 package io.dialob.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Answer implements Serializable {
 
   private String id;
 
+  @JsonInclude(JsonInclude.Include.ALWAYS)
   private Object value;
+
+  private Date updated;
+
+  private String userId;
 
   public Answer withId(String id) {
     this.id = id;
@@ -53,6 +61,22 @@ public class Answer implements Serializable {
     this.value = value;
   }
 
+  public Date getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(Date updated) {
+    this.updated = updated;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
   @Override
   public boolean equals(Object obj) {
     if (obj == null || !(obj instanceof Answer)) {
@@ -65,16 +89,24 @@ public class Answer implements Serializable {
     return new EqualsBuilder()
       .append(id, other.getId())
       .append(value, other.getValue())
+      .append(updated, other.getUpdated())
+      .append(userId, other.getUserId())
       .isEquals();
   }
 
   @Override
   public int hashCode() {
-    return (id != null ? id.hashCode() : 0) * 31 + (value != null ? value.hashCode() : 0);
+    return ( ( ( (id != null ? id.hashCode() : 0) * 31
+          + (value != null ? value.hashCode() : 0)) * 31
+          + (updated != null ? updated.hashCode() : 0)) * 31
+          + (userId != null ? userId.hashCode() : 0)) * 31;
   }
 
   @Override
   public String toString() {
-    return "{\"id\":\"" + id + "\",\"value\":\"" + value + "\"}";
+    return "{\"id\":\"" + id + "\"," +
+      "\"value\":" + (value == null ? "null" : "\"" + value + "\"") + "," +
+      "\"updated\":" + (updated == null ? "null" : "\"" + updated + "\"") + "," +
+      "\"userId\":" + (userId == null ? "null" : "\"" + userId + "\"") + "}";
   }
 }
