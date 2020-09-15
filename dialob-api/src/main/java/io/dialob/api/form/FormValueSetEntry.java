6044eb57f12e533b251aa1ff92547c6d76ed311a
Tomi Pakarinen <tomi.pakarinen@resys.io>
2020-09-15 12:40:30 +0300

    Added optional when condition on value set entry

--- dialob-api/src/main/java/io/dialob/api/form/FormValueSetEntry.java
+++ dialob-api/src/main/java/io/dialob/api/form/FormValueSetEntry.java
@@ -5,18 +5,21 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.dialob.api.annotation.AllowNulls;
+import io.dialob.api.annotation.Nullable;
 import org.immutables.gson.Gson;
 import org.immutables.value.Value;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Optional;
 
 @Value.Immutable
 @Value.Style(jdkOnly = true)
 @Value.Modifiable
 @JsonSerialize(as = ImmutableFormValueSetEntry.class)
 @JsonDeserialize(as = ImmutableFormValueSetEntry.class)
+@JsonInclude(content = JsonInclude.Include.NON_NULL, value = JsonInclude.Include.NON_NULL)
 @Gson.TypeAdapters(emptyAsNulls = true)
 public interface FormValueSetEntry extends Serializable {
 
@@ -25,10 +28,15 @@ public interface FormValueSetEntry extends Serializable {
 
   Map<String, String> getLabel();
 
+  @Nullable
+  String getWhen();
+
   @JsonInclude
   @JsonAnyGetter
   @AllowNulls
   @Gson.Ignore
   Map<String, Object> getAdditionalProperties();
+
+
 
 }
